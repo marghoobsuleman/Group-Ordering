63a7128ec67a215cd911767686bd573861494023
saurabh.pal2 <saurabh.pal2@jublfood.com>
2023-02-13 23:42:26 +0530

    events added on server and client

--- poc/server/server.js
+++ poc/server/server.js
@@ -1,5 +1,6 @@
 const http = require("http");
 const socket = require("socket.io");
+const {v4 : uuidv4} = require('uuid')
 const httpServer = http.createServer();
 const io = socket(httpServer, {
   cors: {
@@ -7,35 +8,84 @@ const io = socket(httpServer, {
   },
 });
 
-let groups = [];
+const activeGroups = new Map(); // contains total groups created
 
 io.on("connection", (socket) => {
   const id = socket.id;
+  
   socket.on("create group", () => {
-    io.to(id).emit("group created", { id });
-    groups.push(id);
-    console.log("group created", id);
+    const groupId = uuidv4();
+    socket.join(groupId);
+    const group = {
+      ownerId: id,
+      live: 1,
+      cart: null
+    };
+
+    activeGroups.set(groupId, group);
+    io.to(id).emit("group created", { groupId: groupId, ownerId: id, live: 1 });
+    console.log("group created with groupId", groupId);
+    console.log("Group Map:", activeGroups);
   });
 
-  socket.on("join group", ({ id: groupID }) => {
-    if(groups.includes(groupID)) {
-      socket.join(groupID);
-      io.to(id).emit("group joined");
+  socket.on("join group", ({groupId: groupId }) => {
+    if(activeGroups.has(groupId)) {
+        // group exist
+      socket.join(groupId);
+      const group = activeGroups.get(groupId);
+      group.live += 1;
+      activeGroups.set(groupId, group);
+      io.to(id).emit("group joined", {live: group.live, ownerId: group.ownerId, cart: group.cart});
+      socket.broadcast.to(groupId).emit("new join info", {live: group.live});
+      console.log("Group Map:", activeGroups);
     } else {
-      io.to(id).emit("group not exist");
+        // group doest not exist
+        io.to(id).emit("group not exist");
     }
-  })
+  });
 
-  socket.on("send to group", ({ text, groupID }) => {
-    io.to(groupID).emit("text", { text });
+  socket.on("leave group", ({groupId: groupId, ownerId: ownerId}) => {
+    if(groupId == null) {
+      console.log("Non Participants disconnected");
+      return;
+    }
+    if(!activeGroups.has(groupId)) {
+      console.log("Group does not exist");
+      return;
+    }
+    if(activeGroups.get(groupId).ownerId == ownerId) {
+      // owner left the group --> delete entire group
+      activeGroups.delete(groupId);
+      socket.broadcast.to(groupId).emit("owner left", "This group does not exist");
+      io.to(id).emit("owner left", "You deleted this group.");
+      console.log("Group has been deleted:", groupId);
+    } else {
+      // participant left the group --> live--
+      const group = activeGroups.get(groupId);
+      group.live -= 1;
+      activeGroups.set(id, group);
+      socket.broadcast.to(groupId).emit("participant left", {live: group.live});
+      io.to(id).emit("owner left", "Sorry to see you go.");
+      console.log("participant left the group", id);
+    }
+    console.log("Group Map:", activeGroups);
   })
 
   socket.on("disconnect", () => {
-    groups = groups.filter(_ => _ !== id);
-  })
+    console.log("user disconnected");
+  });
+
+  socket.on("emit update cart", ({ groupId: groupId, cart: items}) => {
+    const group = activeGroups.get(groupId);
+    group.cart = items;
+    socket.broadcast.to(groupId).emit("listen update cart", {cart: group.cart});
+    console.log("Group Map:", activeGroups);
+  });
 
   console.log("connected", id);
+  console.log("Group Map:", activeGroups);
 });
+
 
 const PORT = process.env.PORT || 3000;
 
--- poc/src/App.vue
+++ poc/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>Chat App</h1>
+  <h1>Group Order</h1>
   <template v-if="!joinedGroup">
     <button @click="createGroup">Create group</button>
     <br/>
@@ -9,20 +9,57 @@
   <template v-else>
     <h1>Welcome to the group {{ groupID }}</h1>
     <code v-for="itext in texts"> {{ itext }} <br/></code>
-    <textarea v-model="text"></textarea> <br/>
-    <button @click="sendToGroup">Send to group</button>
+    <!-- <textarea v-model="text"></textarea> <br/> -->
+    <button @click="sendToGroup">Update Cart</button><br/>
+    <button @click="leaveGroup">Leave Group</button>
   </template>
 </template>
+
+<!-- <template>
+  <h1>Group Order</h1>
+  <template v-if="groupCreated">
+    <template v-if="!joinedGroup">
+      <h1>Your Group has been created with GroupId: {{ groupID }}</h1><br/>
+      <button @click="goToTheGroup">Go To The Group</button>
+    </template>
+    <template v-else>
+      <h1>Welcome to the group {{ groupID }}</h1>
+      <code v-for="itext in texts"> {{ itext }} <br/></code>
+      <textarea v-model="text"></textarea> <br/>
+      <button @click="sendToGroup">Send to group</button><br/>
+      <button @click="leaveGroup">Leave Group</button>
+    </template>
+  </template>
+  <template v-else-if="joinedGroup">
+    <h1>Welcome to the group {{ groupID }}</h1>
+    <code v-for="itext in texts"> {{ itext }} <br/></code>
+    <textarea v-model="text"></textarea> <br/>
+    <button @click="sendToGroup">Send to group</button>
+    <button @click="leaveGroup">Leave Group</button>
+  </template>
+  <template v-else>
+    <button @click="createGroup">Create group</button>
+    <br/>
+    <input v-model="groupID" type="text" placeholder="Group ID" />
+    <button @click="joinGroup">Join group</button>
+  </template>
+</template> -->
+
 
 <script>
 import socket from "./socket";
 export default {
   data() {
     return {
-      groupID: "",
+      groupID: null,
+      ownerID: null,
       joinedGroup: false,
+      groupCreated: false,
+      iAmTheOwner: false,
+      live: 0,
       text: "",
-      texts: []
+      texts: [],
+      cart: null
     }
   },
   methods: {
@@ -30,32 +67,115 @@ export default {
       socket.emit("create group");
     },
     joinGroup() {
-      socket.emit("join group", {id: this.groupID});
+      socket.emit("join group", {groupId: this.groupID});
+    },
+    leaveGroup() {
+      socket.emit("leave group", {groupId: this.groupID, ownerId: this.ownerID});
     },
     sendToGroup() {
-      if(this.text) {
-        socket.emit("send to group", { text: this.text, groupID: this.groupID });
-      }
+      socket.emit("emit update cart", { groupId: this.groupID, cart: [{menuCode: "PIZ001"},{menuCode: "PIZ002"},{menuCode: "PIZ003"}]});
     },
   },
   created() {
-    socket.on("group created", ({id}) => {
-      this.groupID = id;
+    socket.on("group created", ({ groupId: groupId, ownerId: id, live: live }) => {
+      // pass userId generated when user loggedIn and it will act as groupId
+      this.groupID = groupId;
+      this.ownerID = id;
       this.joinedGroup = true;
-      console.log("groupid", id);
+      this.groupCreated = true;
+      this.iAmTheOwner = true;
+      this.live = live;
+      const infoGroup = {
+        groupId: this.groupID,
+        ownerId: this.ownerID,
+        joinedGroup: this.joinedGroup,
+        groupCreated: this.groupCreated,
+        iAmTheOwner: this.iAmTheOwner,
+        live: this.live,
+        cart: this.cart
+      }
+      console.log("Your Group has been created.");
+      console.log("group info:", infoGroup);
     })
 
-    socket.on("group joined", () => {
+    socket.on("group joined", ({ live: live, ownerId: ownerId, cart: cart }) => {
       this.joinedGroup = true;
+      this.live = live;
+      this.ownerID = ownerId;
+      this.cart = cart;
+      const infoGroup = {
+        groupId: this.groupID,
+        ownerId: this.ownerID,
+        joinedGroup: this.joinedGroup,
+        groupCreated: this.groupCreated,
+        iAmTheOwner: this.iAmTheOwner,
+        live: this.live,
+        cart: this.cart
+      }
+      console.log("Welcome to the group");
+      console.log("group info:", infoGroup);
+    })
+
+    socket.on("new join info", ({ live: live }) => {
+      this.live = live;
+      const infoGroup = {
+        groupId: this.groupID,
+        ownerId: this.ownerID,
+        joinedGroup: this.joinedGroup,
+        groupCreated: this.groupCreated,
+        iAmTheOwner: this.iAmTheOwner,
+        live: this.live,
+        cart: this.cart
+      }
+      console.log("New person joined the group");
+      console.log("group info:", infoGroup);
+    })
+
+    socket.on("owner left", (message) => {
+      this.groupCreated = false;
+      this.joinedGroup = false;
+      this.ownerID = null;
+      this.iAmTheOwner = false;
+      this.groupID = null;
+      this.live = 0;
+      this.cart = null;
+      const infoGroup = {
+        groupId: this.groupID,
+        ownerId: this.ownerID,
+        joinedGroup: this.joinedGroup,
+        groupCreated: this.groupCreated,
+        iAmTheOwner: this.iAmTheOwner,
+        live: this.live,
+        cart: this.cart
+      }
+      console.log("message:", message);
+      console.log("group info:", infoGroup);
+    })
+
+    socket.on("participant left", ({live: live}) => {
+      // any other participant left the group
+      this.live = live;
+      const infoGroup = {
+        groupId: this.groupID,
+        ownerId: this.ownerID,
+        joinedGroup: this.joinedGroup,
+        groupCreated: this.groupCreated,
+        iAmTheOwner: this.iAmTheOwner,
+        live: this.live
+      }
+      console.log("One Person left the group");
+      console.log("group info:", infoGroup);
     })
 
     socket.on("group not exist", () => {
       console.log("group does not exisit");
     })
 
-    socket.on("text", ({ text }) => {
-      this.texts.push(text);
+    socket.on("listen update cart", ({cart: cart}) => {
+      this.texts.push(cart);
+      console.log("cartItems:", cart);
     })
+
   }
 }
 </script>
